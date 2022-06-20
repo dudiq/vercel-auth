b08ff91c2b24e585df15558020b7a8347cc250d4
dudiq <idudiq@gmail.com>
2022-06-20 09:40:48 +0300

    fix: update server dest

--- apps/remix-app/remix.config.js
+++ apps/remix-app/remix.config.js
@@ -5,10 +5,11 @@ module.exports = {
   serverBuildTarget: "vercel",
   appDirectory: "app",
   ignoredRouteFiles: [".*"],
-  assetsBuildDirectory: "public/build",
+  // assetsBuildDirectory: "api/build",
+  // serverBuildPath: "api/build",
   // When running locally in development mode, we use the built in remix
   // server. This does not understand the vercel lambda module format,
   // so we default back to the standard build output.
-  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
+  server: process.env.NODE_ENV === "development" ? undefined : "./api/server.js",
   serverDependenciesToBundle: [/.*/],
 };
